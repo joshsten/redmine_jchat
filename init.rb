87f7fb777a1b684e6ce84ae74411a21eed649fd7
root <root@ubuntu.ubuntu-domain>
2010-11-26 01:07:45 -0600

    Added Localization Support.

--- init.rb
+++ init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_jchat do
   name 'Redmine JChat'
   author 'Joshua Enfield'
   description 'Provides a team chat similiar to Google\'s/Facebooks chat.'
-  version '0.1.0'
+  version '0.2.0'
   settings(:partial => 'settings/chat')
 end
